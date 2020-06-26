5a07a3f95d3cf4aa363e543ce6c38cf43084b317
Charu Chandak <imcharuchandak27@gmail.com>
2020-06-26 23:21:07 +0530

    Update index.php
    done

--- index.php
+++ index.php
@@ -2,7 +2,4 @@
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
- "This ", "string ", "was ", "made ", "with multiple parameters."
-   
-   makeing a mistake intentionally
 ?>
