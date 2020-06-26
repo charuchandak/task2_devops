ce91effe3f2627b92178eb7849038347cd7d4bdb
Charu Chandak <imcharuchandak27@gmail.com>
2020-06-26 21:51:09 +0530

    Update index.php
    commit fail

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+ "This ", "string ", "was ", "made ", "with multiple parameters."
+   
+   makeing a mistake intentionally
 ?>
